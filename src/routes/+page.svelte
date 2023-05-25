d6349401fd25096178782e646e3d15a833e17294
TheWisePigeon <zozozozeph@gmail.com>
2023-05-25 22:37:29 +0000

    init app with tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="bg-red-300 text-2xl" >Tailwind for the win bitched</h1>
