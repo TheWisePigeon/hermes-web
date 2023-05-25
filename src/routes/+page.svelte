796a8075e4e9f71e5f0f0a2b294d24252112d647
TheWisePigeon <zozozozeph@gmail.com>
2023-05-25 22:54:57 +0000

    e-ui: implementing landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1 class="bg-red-300 text-2xl" >Tailwind for the win bitched</h1>
+<header class="flex justify-between items-center p-3 text-xl">
+	<a href="/" class="font-bold text-2xl">HERMES</a>
+	<div class="flex gap-5">
+		<a href="/docs" class="underline">Docs</a>
+		<a target="_blank" href="https://github.com/Hermes-Auth" class="underline">GitHub</a>
+		<a href="/auth" class="underline">Get started</a>
+	</div>
+</header>
+
+<h1 class="text-center text-3xl font-bold">
+	HERMES is a passwordless authentication API for seamless user verification via email. Simplify
+	authentication and enhance security in your applications.
+</h1>
