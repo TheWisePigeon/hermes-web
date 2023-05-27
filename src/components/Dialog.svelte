15d7d73831372c2b00bc8e3623d0a39d8ebf0199
TheWisePigeon <zozozozeph@gmail.com>
2023-05-27 14:19:28 +0000

    minor updates

---
+++ src/components/Dialog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { show_dialog } from '../lib/stores';
+    function close_dialog(){
+            show_dialog.update(()=>false)
+        }
+</script>
+
+{#if $show_dialog}
+	<div class="fixed inset-0 flex items-center justify-center">
+		<div
+			class={` w-52 h-52 transform ${ $show_dialog?'':'scale-0' } transition-all duration-300 rounded-lg shadow-lg p-4`}
+		>
+			<h1>You are not authenticated. Do so via the authentication form down below and get access to Hermes!</h1>
+            <button on:click={close_dialog}>
+                <h1>Ok</h1>
+            </button>
+		</div>
+	</div>
+{/if}
---
+++ src/lib/stores.ts
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store"
+
+export const show_dialog = writable(false)
+export const dialog_content = writable("")
